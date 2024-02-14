eb332f42e44ca0589fcf8e14744b36dcc71c787f
Your Name <you@example.com>
2024-02-14 20:37:33 +0530

    add filess

--- Tictoe.js
+++ Tictoe.js
@@ -1,6 +1,8 @@
 let boxes=document.querySelectorAll(".box")
 let resetb=document.querySelector("#reset")
-
+let newb=document.querySelector("#new-btn");
+let msgcontain=document.querySelector(".msg-container");
+let msg1=document.querySelector("#msg");
 let turn0=true;
 
 const winPattern=[
@@ -25,8 +27,29 @@ boxes.forEach((box)=>{
             box.innerText="X";
             turn0=true;
         }
+        box.disabled=true;
+        checkWinner();
 
 
     });
 });
+const showWinner=(winner)=>{
+    msg1.innerText="Congratulations, Winner is " + winner;
+     
+};
+const checkWinner=()=>{
+    for(let pat of winPattern){
+        let w1=boxes[pat[0]].innerText;
+        let w2=boxes[pat[1]].innerText;
+        let w3=boxes[pat[2]].innerText;
+
+        if (w1!="" &&w2!="" && w3!=""){
+            if(w1===w2 && w2===w3){
+                console.log("WINNER",w1);
+                showWinner(w1);
+            }
+        }
+    }
+
+}
 
